d0d9f53b61f70da73c03fdcd6ac0c0738e00480c
Mateus <mateuslima565@gmail.com>
2020-03-07 23:10:07 -0300

    chat init

---
+++ Chat/index.js
@@ -0,0 +1,58 @@
+
+const express = require('express')
+const http = require('http')
+const app = express()
+const server = http.createServer(app)
+const io = require('socket.io').listen(server)
+
+app.get('/', (req, res) => {
+
+    res.send('Chat Server is running on port 3000')
+});
+io.on('connection', (socket) => {
+
+    console.log('user connected')
+
+    socket.on('join', function (userNickname) {
+
+        console.log(userNickname + " : has joined the chat ");
+
+        socket.broadcast.emit('userjoinedthechat', userNickname + " : has   joined the chat ");
+    })
+
+
+    socket.on('messagedetection', (senderNickname, messageContent) => {
+
+        //log the message in console 
+
+        console.log(senderNickname + " : " + messageContent)
+
+        //create a message object 
+
+        let message = {
+            "message": messageContent,
+            "senderNickname": senderNickname
+        }
+
+        // send the message to all users including the sender  using io.emit  
+
+        io.emit('message', message)
+
+    })
+
+    socket.on('disconnect', function () {
+
+        console.log(userNickname + ' has left ')
+
+        socket.broadcast.emit("userdisconnect", ' user has left')
+
+    })
+
+})
+
+
+server.listen(3000, () => {
+
+    console.log('Node app is running on port 3000')
+
+})
